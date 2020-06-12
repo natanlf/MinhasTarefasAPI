dd0dc623289f289f72bde60013841b77b699fac4
Natan <natanlaraferreira@gmail.com>
2020-06-12 14:09:58 -0300

    Migration e Identity

--- MinhasTarefasAPI/Database/MinhasTarefasContext.cs
+++ MinhasTarefasAPI/Database/MinhasTarefasContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MinhasTarefasAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace MinhasTarefasAPI.Database
 {
-    public class MinhasTarefasContext : DbContext //DbContext para usar o banco de dados
+    //Rodamos no Console do Gerenciador de Pacotes (está no menu ferramentas) : 
+    //Add-Migration BancoInicial, assim criamos o banco
+    //Após a criação rodar o comando Update-Database
+    public class MinhasTarefasContext : IdentityDbContext<ApplicationUser> //mudamos para IdentityDbContext, pois vamos usar usuário para login
     {
         public MinhasTarefasContext(DbContextOptions<MinhasTarefasContext> options) : base(options)
         {
