921ad65516e7bc4ddeabe47602b9fac53565a90a
Natan <natanlaraferreira@gmail.com>
2020-06-12 15:46:05 -0300

    Tarefa Repository

---
+++ MinhasTarefasAPI/Repositories/TarefaRepository.cs
@@ -0,0 +1,55 @@
+﻿using MinhasTarefasAPI.Database;
+using MinhasTarefasAPI.Models;
+using MinhasTarefasAPI.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasAPI.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private readonly MinhasTarefasContext _banco;
+        public TarefaRepository(MinhasTarefasContext banco)
+        {
+            _banco = banco;
+        }
+        public List<Tarefa> Restauracao(ApplicationUser usuario, DateTime dataUltimaSincronizacao)
+        {
+            //Converte de IEnumerable para IQueryable. Para mais info.: 
+            /*http://www.macoratti.net/14/11/net_ieiq1.htm*/
+
+            var query = _banco.Tarefas.Where(a => a.UsuarioId == usuario.Id).AsQueryable();
+
+            if (dataUltimaSincronizacao != null) {
+                query.Where(a => a.Criado >= dataUltimaSincronizacao || a.Atualizado >= dataUltimaSincronizacao);
+            }
+
+            return query.ToList<Tarefa>();
+        }
+
+        public List<Tarefa> Sincronizacao(List<Tarefa> tarefas)
+        {
+            var tarefasNovas = tarefas.Where(a => a.IdTarefaApi == 0);
+            // Cadastrar novos registros
+            if (tarefasNovas.Count() > 0) {
+                foreach (var tarefa in tarefasNovas) {
+                    _banco.Tarefas.Add(tarefa);
+                }
+            }
+
+            var tarefasExcluidasAtualizadas = tarefas.Where(a => a.IdTarefaApi != 0);
+            // Atualização de registro (Excluido)
+            if (tarefasExcluidasAtualizadas.Count() > 0) {
+                foreach (var tarefa in tarefasExcluidasAtualizadas) {
+                    _banco.Tarefas.Update(tarefa);
+                }
+            }
+
+            _banco.SaveChanges();
+
+            return tarefasNovas.ToList();
+        }
+    }
+}
